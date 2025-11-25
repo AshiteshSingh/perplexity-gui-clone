e7626b83ab2107a705f9d3678f716506fcaa8525
Ashitesh Singh <raghuvesh1285@gmail.com>
2025-11-25 18:07:38 +0530

    Update check_exports.js

--- check_exports.js
+++ check_exports.js
@@ -1,9 +1,6 @@
 const aiReact = require('@ai-sdk/react');
 console.log('Exports:', Object.keys(aiReact));
 try {
-    // Mock useChat to see what it returns (if possible, or just check prototype/definitions if exposed)
-    // Since we can't easily run hooks outside components, we'll just check if we can require it.
-    // Actually, let's try to inspect the module more deeply.
     console.log('useChat type:', typeof aiReact.useChat);
 } catch (e) {
     console.error(e);
